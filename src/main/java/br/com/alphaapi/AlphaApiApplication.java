0ff26f21e1153e2eb48e80cd9f6f2e0951d51ea2
lucasramalho.s97 <lucasramalho.s97@gmail.com>
2021-08-16 23:34:52 -0300

    Initial Commit

---
+++ src/main/java/br/com/alphaapi/AlphaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alphaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaApiApplication.class, args);
+	}
+
+}
